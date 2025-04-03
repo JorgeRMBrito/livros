2b38c49bfcad3a28ef81cf010128d3e43d02b5e6
JorgeRMBrito <jorgermatosbrito@hotmail.com>
2025-04-02 21:34:42 -0300

    PAGINAS AJUSTADAS - COLETAR DADOS DA ALTER2

--- livro2025/scripts/dados.js
+++ livro2025/scripts/dados.js
@@ -1,51 +1,95 @@
-function gravarFormulario () {
+
+
+function gravarFormulario() {
     console.log("inicio");
     document.getElementById('formulario_1').addEventListener('submit', function(event) {
-            event.preventDefault(); // Impede o envio padrão do formulário
+      event.preventDefault(); // Impede o envio padrão do formulário
         
-            console.log("foi");
-            // Obtendo os valores dos campos
-                const sup_dia = document.getElementById('sup_dia').value;
-                const med_dia = document.getElementById('med_dia').value;
-                const adjunto = document.getElementById('adjunto').value;
-                const cmte_guarda = document.getElementById('cmte_guarda').value;
-                const encarregado = document.getElementById('encarregado').value;
-                const agua = document.getElementById('agua').value;
-                const gerador = document.getElementById('gerador').value;
-                const energia = document.getElementById('energia').value;
-                const morgue = document.getElementById('morgue').value;
-                const elevador = document.getElementById('elevador').value;
-                const cofre = document.getElementById('cofre').value;
-                const oxigenio = document.getElementById('encarregado').value;
+      
+      // Obtendo os valores dos campos
+      const sup_dia = document.getElementById('sup_dia').value;
+      const med_dia = document.getElementById('med_dia').value;
+      const adjunto = document.getElementById('adjunto').value;
+      const cmte_guarda = document.getElementById('cmte_guarda').value;
+      const encarregado = document.getElementById('encarregado').value;    
+      const cofre = document.getElementById('cofre').value;
+      const oxigenio = document.getElementById('oxigenio').value;
+      
+      // Obtendo os valores dos campos de seleção
 
-               
-             console.log(sup_dia, med_dia, adjunto, cmte_guarda, encarregado, agua, gerador, energia, morgue, elevador);
-            
-                // Criando um objeto para armazenar os dados
-                const dados = {
-                    sup_dia: sup_dia,
-                    med_dia: med_dia,
-                    adjunto: adjunto,
-                    cmte_guarda: cmte_guarda,
-                    encarregado: encarregado,
-                    agua: agua,
-                    gerador: gerador,
-                    energia: energia,
-                    morgue: morgue,
-                    elevador: elevador,
-                    cofre: cofre,
-                    oxigenio: oxigenio
-                };
+      var radioAgua = document.getElementsByName ("agua");
+      for (var i = 0; i < radioAgua.length; i ++) {
+        if (radioAgua[i].checked) {
+          var agua = radioAgua[i].value;
+          }
+        }  
 
-                // Armazenando os dados no localStorage
-            localStorage.setItem('dadosFormulario', JSON.stringify(dados));
-
-            // Exibindo uma mensagem de sucesso
-            alert('Dados salvos com sucesso!');
-            
-            // Opcional: Limpar o formulário após o envio
-           // document.getElementById(formulario).reset();
-        });
+      var radioEnergia = document.getElementsByName ("energia");
+      for (var i = 0; i < radioEnergia.length; i ++) {
+        if (radioEnergia[i].checked) {
+          var energia = radioEnergia[i].value;
+          }
+        }
         
-    } 
+      var radioGerador = document.getElementsByName ("gerador");
+      for (var i = 0; i < radioGerador.length; i ++) {
+          if (radioGerador[i].checked) {
+            var gerador = radioGerador[i].value;
+            }
+          }
+
+      var radioMorgue = document.getElementsByName ("morgue");
+      for (var i = 0; i < radioMorgue.length; i ++) {
+        if (radioMorgue[i].checked) {
+          var morgue = radioMorgue[i].value;
+        }
+      } 
+
+      var radioElevador = document.getElementsByName ("elevador");
+      for (var i = 0; i < radioElevador.length; i ++) {
+        if (radioElevador[i].checked) {
+          var elevador = radioElevador[i].value;
+        }
+      } 
+
+              
+        /*
+        
+        
+        const elevador = document.getElementsByName('elevador').value; */
+
+        console.log(sup_dia, med_dia, adjunto, cmte_guarda, encarregado, energia, agua, gerador, elevador, morgue);
+            
+        // Criando um objeto para armazenar os dados
+        const dados = {
+          sup_dia: sup_dia,
+          med_dia: med_dia,
+          adjunto: adjunto,
+          cmte_guarda: cmte_guarda,
+          encarregado: encarregado,
+          agua: agua,
+          gerador: gerador,
+          energia: energia,
+          morgue: morgue,
+          elevador: elevador,
+          cofre: cofre,
+          oxigenio: oxigenio
+        };
+
+        // Armazenando os dados no localStorage
+        localStorage.setItem('dadosFormulario', JSON.stringify(dados));
+
+        // Exibindo uma mensagem de sucesso
+        alert('Dados salvos com sucesso!');
+            
+        // Opcional: Limpar o formulário após o envio
+       document.getElementById("formulario_1").reset();
+
+      //fecharLink();
+   
+  });
+ abreLink ('alter_of_pg2.html', 'iframe');
+
+
+} 
   
--- livro2025/scripts/script.js
+++ livro2025/scripts/script.js
@@ -1,41 +1,15 @@
 
-// Função para abrir o link
-//inicio
-function abreLink(link, id) {
-       if (typeof controle == 'undefined') {// verifica se a variável controle foi definida. 
-              criarLink(link, id);
-              controle = true;
-              console.log(controle);
-              
-       }
-       else {
-              fecharLink();
-              criarLink(link, id);
-              console.log(controle);
-       }
-        
-        
-
-}
-
 //cria iframe
 function criarLink(link, id) {
   var div = document.getElementById(id);
   var newdiv = document.createElement('iframe');
-  newdiv.src = link;
   var iframe = div.appendChild(newdiv);
+  newdiv.src = link;
   var IncluirId = document.querySelector ('iframe');
-  IncluirId.id = 'conteudo';
+ IncluirId.id = 'conteudo';
+    
  
 }
-
-//fecha iframe
-function fecharLink() {
-  var div = document.getElementById('conteudo');
-  div.remove();
-  
-}
-//fim
 
 //função destacar item do menu
 // inicio
@@ -50,7 +24,7 @@ function NaoDestacar(obj)
 }
 //fim
 
-
+//FUNÇÕES DO ALTERAÇÃO 2
 //CRUD input no alteraçao2
 var line = 1;
   
@@ -88,3 +62,99 @@ function removeInput(id) {
     }
   }   
 }
+
+//FUNÇÃO PARA PEGAR OS DADOS DO FORMULÁRIO E GRAVAR NO LOCAL STORAGE
+// A função é chamada quando o formulário é enviado
+
+function gravarFormulario() {
+  console.log("inicio");
+  document.getElementById('formulario_1').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+      
+    
+    // Obtendo os valores dos campos
+    const sup_dia = document.getElementById('sup_dia').value;
+    const med_dia = document.getElementById('med_dia').value;
+    const adjunto = document.getElementById('adjunto').value;
+    const cmte_guarda = document.getElementById('cmte_guarda').value;
+    const encarregado = document.getElementById('encarregado').value;    
+    const cofre = document.getElementById('cofre').value;
+    const oxigenio = document.getElementById('oxigenio').value;
+    
+    // Obtendo os valores dos campos de seleção
+
+    var radioAgua = document.getElementsByName ("agua");
+    for (var i = 0; i < radioAgua.length; i ++) {
+      if (radioAgua[i].checked) {
+        var agua = radioAgua[i].value;
+        }
+      }  
+
+    var radioEnergia = document.getElementsByName ("energia");
+    for (var i = 0; i < radioEnergia.length; i ++) {
+      if (radioEnergia[i].checked) {
+        var energia = radioEnergia[i].value;
+        }
+      }
+      
+    var radioGerador = document.getElementsByName ("gerador");
+    for (var i = 0; i < radioGerador.length; i ++) {
+        if (radioGerador[i].checked) {
+          var gerador = radioGerador[i].value;
+          }
+        }
+
+    var radioMorgue = document.getElementsByName ("morgue");
+    for (var i = 0; i < radioMorgue.length; i ++) {
+      if (radioMorgue[i].checked) {
+        var morgue = radioMorgue[i].value;
+      }
+    } 
+
+    var radioElevador = document.getElementsByName ("elevador");
+    for (var i = 0; i < radioElevador.length; i ++) {
+      if (radioElevador[i].checked) {
+        var elevador = radioElevador[i].value;
+      }
+    } 
+
+            
+      /*
+      
+      
+      const elevador = document.getElementsByName('elevador').value; */
+
+      console.log(sup_dia, med_dia, adjunto, cmte_guarda, encarregado, energia, agua, gerador, elevador, morgue);
+          
+      // Criando um objeto para armazenar os dados
+      const dados = {
+        sup_dia: sup_dia,
+        med_dia: med_dia,
+        adjunto: adjunto,
+        cmte_guarda: cmte_guarda,
+        encarregado: encarregado,
+        agua: agua,
+        gerador: gerador,
+        energia: energia,
+        morgue: morgue,
+        elevador: elevador,
+        cofre: cofre,
+        oxigenio: oxigenio
+      };
+
+      // Armazenando os dados no localStorage
+      localStorage.setItem('dadosFormulario', JSON.stringify(dados));
+
+      // Exibindo uma mensagem de sucesso
+      alert('Dados salvos com sucesso!');
+          
+      // Opcional: Limpar o formulário após o envio
+     document.getElementById("formulario_1").reset();
+
+     location.replace('alter_of_pg2.html');
+ 
+});
+
+
+
+} 
